f4c2af568b01296fb9728bd4f2b7adc9ab5bf2dd
Cristae-25 <christeh@gmail.com>
2026-07-27 13:33:54 -0700

    Search w/ query testing

--- src/search.py
+++ src/search.py
@@ -1,10 +1,12 @@
 import json
+import random
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams, PointStruct
 from sentence_transformers import SentenceTransformer
 
 COLLECTION_NAME = "scifact_minilm"
 CORPUS_PATH = "data/scifact/corpus.jsonl"
+QUERIES_PATH = "data/scifact/queries.jsonl"
 UPSERT_BATCH_SIZE = 256
 
 client = QdrantClient(url="http://localhost:6333")
@@ -22,11 +24,21 @@ def search(query_text, k=5):
 
 
 if __name__ == "__main__":
-    query = input("Search Query: ")
-    results = search(query)
+    # Number of queries to test
+    K = 5
 
-    for result in results:
+    with open(QUERIES_PATH, "r") as f:
+        all_queries = [json.loads(line) for line in f]
+
+    sampled_queries = random.sample(all_queries, K)
+
+    for query in sampled_queries:
+        query_text = query["text"]
+        results = search(query_text)
+
+        result = results[0]
+        print()
+        print("Query: ", query_text)
         print("Point ID: ", result.id, "\tResult Score: ", result.score)
         print("Title: ", result.payload["title"][:150])  # type: ignore
         print("Text: ", result.payload["text"][:150])  # type: ignore
-        print()
